ddf16154d8dc8d1d1923e1ae0cf027b4fcdffcfb
hitman-max <hitmanguy1238@gmail.com>
2025-05-25 23:00:16 +0000

    done

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+   devIndicators: false,
+   eslint: {
+    ignoreDuringBuilds: true,
+  },
   /* config options here */
 };
 
